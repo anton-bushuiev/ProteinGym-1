32e8fb8171e4c827a84c97d8bdf048ec99d73588
Anton Bushuiev <anton.bushuiev@gmail.com>
2025-11-25 00:45:56 +0100

    Add config for priority_viruses

--- scripts/scoring_DMS_zero_shot/scoring_SaProt_substitutions.sh
+++ scripts/scoring_DMS_zero_shot/scoring_SaProt_substitutions.sh
@@ -3,20 +3,21 @@
 #SBATCH --job-name=scoring_SaProt_substitutions
 #SBATCH --output=scoring_SaProt_substitutions.out
 #SBATCH --error=scoring_SaProt_substitutions.err
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --partition=qgpu
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 
-cd ${SLURM_SUBMIT_DIR}
+cd ${WORK}/ttt/ProteinGym-ProteinTTT/scripts/scoring_DMS_zero_shot
 
 eval "$(conda shell.bash hook)"
 conda activate SaProt
 
 # 1. Dataset config
 
-source ../zero_shot_config.sh
-# source ../zero_shot_config_priority_viruses.sh
+# source ../zero_shot_config.sh
+source ../zero_shot_config_priority_viruses.sh
+# source ../zero_shot_config_priority_viruses_who.sh
 
 # 2. Model config
 
@@ -25,23 +26,23 @@ source ../zero_shot_config.sh
 # export foldseek_bin="/scratch/project/open-35-8/antonb/software/foldseek/bin/foldseek" #(Download from here: https://github.com/steineggerlab/foldseek?tab=readme-ov-file)
 # export protein_ttt_cfg="null"
 
-export SaProt_model_path="/scratch/project/open-35-8/antonb/models/SaProt_650M_AF2" #Path where you have downloaded all SaProt model/tokenizer files from the HF hub (https://huggingface.co/westlake-repl/SaProt_650M_AF2)
-export output_scores_folder="${DMS_output_score_folder_subs}/SaProt-650M"
-export foldseek_bin="/scratch/project/open-35-8/antonb/software/foldseek/bin/foldseek" #(Download from here: https://github.com/steineggerlab/foldseek?tab=readme-ov-file)
-export protein_ttt_cfg="null"
+# export SaProt_model_path="/scratch/project/open-35-8/antonb/models/SaProt_650M_AF2" #Path where you have downloaded all SaProt model/tokenizer files from the HF hub (https://huggingface.co/westlake-repl/SaProt_650M_AF2)
+# export output_scores_folder="${DMS_output_score_folder_subs}/SaProt-650M"
+# export foldseek_bin="/scratch/project/open-35-8/antonb/software/foldseek/bin/foldseek" #(Download from here: https://github.com/steineggerlab/foldseek?tab=readme-ov-file)
+# export protein_ttt_cfg="null"
 
 # export SaProt_model_path="/scratch/project/open-35-8/antonb/models/SaProt_35M_AF2" #Path where you have downloaded all SaProt model/tokenizer files from the HF hub (https://huggingface.co/westlake-repl/SaProt_650M_AF2)
 # export output_scores_folder="${DMS_output_score_folder_subs}/SaProt-35M-ProteinTTT"
 # export foldseek_bin="/scratch/project/open-35-8/antonb/software/foldseek/bin/foldseek" #(Download from here: https://github.com/steineggerlab/foldseek?tab=readme-ov-file)
 # export protein_ttt_cfg=${PROTEINGYM_CACHE}/proteinttt_configs/SaProt-35M.yaml
 
-# export SaProt_model_path="/scratch/project/open-35-8/antonb/models/SaProt_650M_AF2" #Path where you have downloaded all SaProt model/tokenizer files from the HF hub (https://huggingface.co/westlake-repl/SaProt_650M_AF2)
-# export output_scores_folder="${DMS_output_score_folder_subs}/SaProt-650M-ProteinTTT"
-# export foldseek_bin="/scratch/project/open-35-8/antonb/software/foldseek/bin/foldseek" #(Download from here: https://github.com/steineggerlab/foldseek?tab=readme-ov-file)
-# export protein_ttt_cfg=${PROTEINGYM_CACHE}/proteinttt_configs/SaProt-650M.yaml
+export SaProt_model_path="/scratch/project/open-35-8/antonb/models/SaProt_650M_AF2" #Path where you have downloaded all SaProt model/tokenizer files from the HF hub (https://huggingface.co/westlake-repl/SaProt_650M_AF2)
+export output_scores_folder="${DMS_output_score_folder_subs}/SaProt-650M-ProteinTTT"
+export foldseek_bin="/scratch/project/open-35-8/antonb/software/foldseek/bin/foldseek" #(Download from here: https://github.com/steineggerlab/foldseek?tab=readme-ov-file)
+export protein_ttt_cfg=${PROTEINGYM_CACHE}/proteinttt_configs/SaProt-650M.yaml
 
 for DMS_index in $(seq 0 216); do
-    python ../../proteingym/baselines/saprot/compute_fitness_copy.py \
+    python ../../proteingym/baselines/saprot/compute_fitness.py \
         --foldseek_bin ${foldseek_bin} \
         --SaProt_model_name_or_path ${SaProt_model_path} \
         --DMS_reference_file_path ${DMS_reference_file_path_subs} \
---
+++ scripts/zero_shot_config_priority_viruses_who.sh
@@ -0,0 +1,58 @@
+# This file has all general filepaths and directories used in the scoring pipeline. The individual scripts may have 
+# additional parameters specific to each method 
+# See here for further details: https://github.com/OATML-Markslab/ProteinGym/issues/63
+
+# Most of these files can be automatically downloaded 
+# using the `proteingym.utils.download.download_resources()` function.
+# If so, you can set `PROTEINGYM_CACHE` here 
+# to the same as `proteingym.utils.download.PROTEINGYM_CACHE`
+# By default, this will be: "$HOME/.cache/ProteinGym"    
+export PROTEINGYM_CACHE="/scratch/project/open-35-8/data/priority-viruses/data"    
+
+# ---- DMS zero-shot parameters ---- #
+
+# Folders containing the csvs with the variants for each DMS assay
+## DMS benchmark - Substitutions	1.0GB	DMS_ProteinGym_substitutions.zip
+export DMS_data_folder_subs="${PROTEINGYM_CACHE}/who_virus_substitutions" 
+## DMS benchmark - Indels	200MB	DMS_ProteinGym_indels.zip
+# export DMS_data_folder_indels="${PROTEINGYM_CACHE}/DMS_ProteinGym_indels/"
+
+# Folders containing multiple sequence alignments and MSA weights for all DMS assays
+## Multiple Sequence Alignments (MSAs) for DMS assays	5.2GB	DMS_msa_files.zip
+# export DMS_MSA_data_folder="${PROTEINGYM_CACHE}/DMS_msa_files/"
+## Redundancy-based sequence weights for DMS assays	200MB	DMS_msa_weights.zip
+# export DMS_MSA_weights_folder="${PROTEINGYM_CACHE}/DMS_msa_weights/"
+
+# Reference files for substitution and indel assays
+## Included in GitHub repo
+export DMS_reference_file_path_subs="${PROTEINGYM_CACHE}/reference_files/who_virus_reference_proteingym_format.csv"
+## Included in GitHub repo
+# export DMS_reference_file_path_indels=../../reference_files/DMS_indels.csv
+
+# Folders where fitness predictions for baseline models are saved 
+## Zero-shot DMS Model scores - Substitutions	31GB	zero_shot_substitutions_scores.zip
+export DMS_output_score_folder_subs="${PROTEINGYM_CACHE}/zero_shot_substitutions_scores/"
+## Zero-shot DMS Model scores - Indels	5.2GB	zero_shot_indels_scores.zip
+# export DMS_output_score_folder_indels="${PROTEINGYM_CACHE}/zero_shot_indels_scores/"
+
+# Folder containing EVE models for each DMS assay
+## This is where you would store your local copy of trained EVE models 
+## (eg., needed for scoring mutated sequences with EVE or TranceptEVE). 
+## Note that given the substantial size of all EVE model checkpoints that is not something that we make available for download by default.
+## But the code to train these models from scratch is provided here:
+## https://github.com/OATML-Markslab/ProteinGym/blob/main/scripts/scoring_DMS_zero_shot/training_EVE_models.sh
+export DMS_EVE_model_folder="${PROTEINGYM_CACHE}/DMS_EVE_models/"
+
+# Folders containing merged score files for each DMS assay
+## you can specify any location of your choosing. This is where merged files will be stored if you run 
+## the score merge script (https://github.com/OATML-Markslab/ProteinGym/blob/main/scripts/scoring_DMS_zero_shot/merge_all_scores.sh).
+## This merge script takes scores from all baselines in DMS_output_score_folder_subs (the ones we provide,
+## plus new ones you may have computed yourself), and creates individual files per assay that includes
+## scores for all baselines.
+# export DMS_merged_score_folder_subs="${PROTEINGYM_CACHE}/merged_scores/subs/"
+## Same thing for indels
+# export DMS_merged_score_folder_indels="${PROTEINGYM_CACHE}/merged_scores/indels/"
+
+# Folders containing predicted structures for the DMSs (AF2=AlphaFold2)
+## Predicted 3D structures from inverse-folding models	84MB	ProteinGym_AF2_structures.zip
+export DMS_structure_folder="${PROTEINGYM_CACHE}/who_virus_structures/"
